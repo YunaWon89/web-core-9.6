505ad339eb22285c56b76e4e07501e77fcdb6688
Yuna Won <wonyoona31@gmail.com>
2026-03-27 03:24:47 +0900

    cards

--- src/js/index.js
+++ src/js/index.js
@@ -131,17 +131,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  // toggleBtn.addEventListener("click", () => {
-  //   if (text.textContent === "Показать все") {
-  //     tabletSlides.forEach((el) => el.classList.remove("tablet-none"));
-  //     pcSlides.forEach((el) => el.classList.remove("pc-none"));
-  //   } else {
-  //     tabletSlides.forEach((el) => el.classList.add("tablet-none"));
-  //     pcSlides.forEach((el) => el.classList.add("pc-none"));
-  //   }
-  // })
 
-  // стартовое состояние: "Показать все" (то есть скрыто)
   let hidden = true
   setHidden(hidden)
 
@@ -241,8 +231,56 @@ function initSwiper3() {
     }
 }
 
-// Запускаем при загрузке страницы
-document.addEventListener('DOMContentLoaded', initSwiper3);
 
-// И при изменении размера окна
-window.addEventListener('resize', initSwiper3);
+
+document.addEventListener("DOMContentLoaded", function () {
+  let swiper = null;
+  function initSwiper() {
+    if (window.innerWidth < 768 && swiper === null) {
+      swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    }
+
+    if (window.innerWidth >= 768 && swiper !== null) {
+      swiper.destroy(true, true);
+      swiper = null;
+    }
+  }
+  initSwiper();
+  window.addEventListener("resize", initSwiper);
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.querySelector(".show-all");
+  const text = toggleBtn.querySelector(".text");
+  const hiddenSlides = document.querySelectorAll(".tablet-none, .pc-none");
+
+  let hidden = true;
+
+  function setHidden() {
+    hiddenSlides.forEach(el => {
+      if (hidden) {
+        el.style.display = "none";
+      } else {
+        el.style.display = "flex";
+      }
+    });
+
+    text.textContent = hidden ? "Показать все" : "Скрыть";
+  }
+
+  setHidden();
+
+  toggleBtn.addEventListener("click", () => {
+    hidden = !hidden;
+    setHidden();
+  });
+});
+
